46b769e155221c3cc5e60765a6a283d506a73df1
bella-gatz <102693135+bella-gatz@users.noreply.github.com>
2025-05-21 17:48:21 -0700

    added base app

---
+++ xpyre/xpyre/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  xpyre
+//
+//  Created by Bella Gatzemeier on 5/21/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
