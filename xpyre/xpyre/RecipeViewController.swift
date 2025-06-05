9b63444c162bb9b9b450f8c0468a7a504397fe25
brandonjinkim <brandonjinkim@gmail.com>
2025-06-05 11:54:01 -0700

    Added firebase dependencies and got AI responses working

--- xpyre/xpyre/RecipeViewController.swift
+++ xpyre/xpyre/RecipeViewController.swift
@@ -6,11 +6,38 @@
 //
 
 import UIKit
+import FirebaseAI
+
+let ai = FirebaseAI.firebaseAI(backend: .googleAI())
+let model = ai.generativeModel(modelName: "gemini-2.0-flash")
+
+// will change this later
+let prompt = "What recipes can i make with three apples?"
 
 class RecipeViewController: UIViewController {
-
+    
+    @IBOutlet weak var testButton: UIButton!
+    @IBAction func generateRecipes() {
+        Task { @MainActor in
+            do {
+                let response = try await model.generateContent(prompt)
+                print(response)
+                aiResponse.text = response.text
+            } catch {
+                print("Error: \(error)")
+            }
+        }
+    }
+    
+    @IBOutlet weak var aiResponse: UILabel!
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+
+        
     }
 }
